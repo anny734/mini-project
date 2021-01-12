2b2af86303091fedd5bf4cfee89e34685ca33450
Ankit Sonje <ankitso@cybage.com>
2021-01-12 17:10:57 +0530

    new changes 12 jan 2

--- js/register.js
+++ js/register.js
@@ -1,8 +1,35 @@
-"use strict";
-$(document).ready(function(){
-    var email = sessionStorage.getItem("emailId");
-    if( email !== ""){
-        document.querySelector("#getEmail").innerHTML=email;
-    }
-         
-});
+   
+    $(function() {
+      
+      $("form[name='register']").validate({
+        rules: {
+          firstname: "required",
+          lastname: "required",
+          email: {
+            required: true,
+            email: true
+          },
+          password: {
+            required: true,
+            minlength: 5
+          },
+          username: {
+            required: true,
+          }
+        },
+        
+        messages: {
+          username: "Please enter username",
+          password: {
+            required: "Please provide a password",
+            minlength: "Your password must be at least 5 characters long"
+          },
+          email: "Please enter a valid email address"
+        },
+      
+        submitHandler: function(form) {
+          form.submit();
+        }
+      });
+    });
+    
